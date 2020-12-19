2bb09dabec971467d0ff98b3b31984436f784f04
thautwarm <twshere@outlook.com>
2020-12-19 10:56:31 +0900

    up

--- mark-and-sweep.cpp
+++ mark-and-sweep.cpp
@@ -7,8 +7,8 @@ struct Runtime;
 struct MemCell;
 using reference = uint8_t*;
 using MemStore = std::vector<MemCell*>;
-using MemToVisit = std::stack<MemCell*>;
-typedef void (*TraverseFn)(reference, MemStore, MemToVisit&);
+using Root = std::stack<MemCell*>;
+typedef void (*TraverseFn)(reference, MemStore, Root&);
 
 struct MemCell{
     TraverseFn mark_f;
@@ -35,13 +35,7 @@ MemCell* allocate(size_t n){
     return cell;
 }
 
-struct Runtime{
-    MemStore heap;
-    Runtime() : heap() {}
-};
-
-void gc(Runtime* rt, MemToVisit& root){
-    auto&& heap = rt->heap;
+void gc(MemStore& heap, Root& root){
     while (!root.empty()){
       auto&& TOS = root.top();
       root.pop();
@@ -63,7 +57,7 @@ void gc(Runtime* rt, MemToVisit& root){
             free(m);
         }
     }
-    rt->heap.swap(newheap);
+    heap.swap(newheap);
 
 }
 
@@ -80,7 +74,7 @@ T* to_buf(MemCell* cell){
 }
 
 template<typename T>
-void mark_f(uint8_t* buf, MemStore heap, MemToVisit& to_visit){}
+void mark_f(uint8_t* buf, MemStore heap, Root& to_visit){}
 
 // =================== test GC =====================
 
@@ -95,7 +89,7 @@ struct BI{
 };
 
 template<>
-void mark_f<OArr>(reference buf, MemStore heap, MemToVisit& to_visit){
+void mark_f<OArr>(reference buf, MemStore heap, Root& to_visit){
 
     auto&& oarr = reinterpret_cast<OArr*>(buf);
     for(size_t&& i = 0; i < oarr->size; ++i){
@@ -104,25 +98,25 @@ void mark_f<OArr>(reference buf, MemStore heap, MemToVisit& to_visit){
     }
 }
 
-reference new_bi(Runtime* rt, int32_t i, int32_t j){
+reference new_bi(MemStore& heap, int32_t i, int32_t j){
    auto&& cell = allocate(8);
    cell->mark_f = mark_f<BI>;
    BI* bi = to_buf<BI>(cell);
    bi->i = i;
    bi->j = j;
-   rt->heap.push_back(cell);
+   heap.push_back(cell);
    return reinterpret_cast<reference>(bi);
 }
 
-reference new_array_of_objs(Runtime* rt, size_t n){
+reference new_array_of_objs(MemStore& heap, size_t n){
    auto&& cell = allocate(8 * n + 8);
    cell->mark_f = mark_f<OArr>;
    auto&& arr = to_buf<OArr>(cell);
    arr->size = n;
    for(size_t i = 0; i < n; i++){
-       arr->elts[i] = new_bi(rt, 1, i);
+       arr->elts[i] = new_bi(heap, 1, i);
    }
-   rt->heap.push_back(cell);
+   heap.push_back(cell);
    return reinterpret_cast<reference>(arr);
 }
 
@@ -130,14 +124,14 @@ int main(){
     printf("header size: %d\n", bufoffset);
     printf("cell size: %d\n", sizeof(MemCell));
 
-    Runtime rt;
-    auto&& oarr = new_array_of_objs(&rt, 4);
+    MemStore heap;
+    auto&& oarr = new_array_of_objs(heap, 4);
     auto&& spec = reinterpret_cast<OArr*>(oarr);
 
-    std::stack<MemCell*> st;
-    st.push(get_cell(spec->elts[0]));
-    printf("Heap size: %d\n", rt.heap.size()); // 5
-    gc(&rt, st);
-    printf("Heap size: %d\n", rt.heap.size()); // 1
+    Root root;
+    root.push(get_cell(spec->elts[0]));
+    printf("Heap size: %d\n", heap.size()); // 5
+    gc(heap, root);
+    printf("Heap size: %d\n", heap.size()); // 1
     return 0;
 }
